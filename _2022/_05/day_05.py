ab61471906847b30017837497315b6f5ea03a739
Fede Calendino <fede@calendino.com>
2022-12-06 21:42:40 +0100

    Day 5: Supply Stacks

--- _2022/_05/day_05.py
+++ _2022/_05/day_05.py
@@ -1,32 +1,3 @@
-def parser(name):
-    lines = reversed(open(f"{name}.txt").readlines())
-
-    actions = []
-
-    for line in lines:
-        if not line.startswith("move"):
-            break
-
-        _, amount, _, source, _, target = line.split(" ")
-        actions.insert(0, (int(amount), int(source) - 1, int(target) - 1))
-
-    count = max(map(int, filter(bool, next(lines).split(" "))))
-
-    stacks = [list() for _ in range(count)]
-
-    for line in lines:
-        for i in range(count):
-            if 4 * i + 1 > len(line):
-                continue
-
-            item = line[4 * i + 1]
-
-            if item != " ":
-                stacks[i].append(item)
-
-    return stacks, actions
-
-
 def part_01(stacks, actions) -> str:
     stacks = [list(stack) for stack in stacks]
 
@@ -49,6 +20,36 @@ def part_02(stacks, actions) -> str:
         stacks[target] += items
 
     return "".join(map(lambda stack: stack[-1], stacks))
+
+
+def parser(name):
+    with open(f"{name}.txt") as file:
+        lines = reversed(file.readlines())
+
+        actions = []
+
+        for line in lines:
+            if not line.startswith("move"):
+                break
+
+            _, amount, _, source, _, target = line.split(" ")
+            actions.insert(0, (int(amount), int(source) - 1, int(target) - 1))
+
+        count = max(map(int, filter(bool, next(lines).split(" "))))
+
+        stacks = [list() for _ in range(count)]
+
+        for line in lines:
+            for i in range(count):
+                if 4 * i + 1 > len(line):
+                    continue
+
+                item = line[4 * i + 1]
+
+                if item != " ":
+                    stacks[i].append(item)
+
+        return stacks, actions
 
 
 test_values = parser("test")
